0b3b0f4fed684d789d2c1089c3a482613ccff4e8
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-17 14:01:20 +0300

    Upload

--- routes/uploadimage.js
+++ routes/uploadimage.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const multer = require('multer');
+const Blog = require("../models/Blog");
+const Referance = require("../models/Referance");
+const Contact = require("../models/Contact");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'images')
+    cb(null, 'routes/images')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
   }
-})
- 
+})  
 var upload = multer({ storage: storage })
 
 // For Single image upload
@@ -22,5 +24,47 @@ router.post('/', upload.single('myFile'), (req, res, next) => {
     res.send(file)
   
 })
+
+router.get('/get-referance/:name', (req, res, next) => {
+    var filename = req.params.name;
+    console.log(filename)
+    const data = Referance.findOne({filePath:{$in:req.params.name}});
+    
+      res.set({'Content-Type': 'image/jpg'});
+      res.contentType('image/png');
+      res.contentType('image/jpg');
+      res.sendFile(__dirname+'/images/'+req.params.name);
+    
+    
+
+});
+
+router.get('/get-blog/:name', (req, res, next) => {
+  var filename = req.params.name;
+  console.log(filename)
+  const data = Blog.findOne({filePath:{$in:req.params.name}});
+  
+    res.set({'Content-Type': 'image/jpg'});
+    res.contentType('image/png');
+    res.contentType('image/jpg');
+    res.sendFile(__dirname+'/images/'+req.params.name);
+  
+  
+
+});
+
+router.get('/get-contact/:name', (req, res, next) => {
+  var filename = req.params.name;
+  console.log(filename)
+  const data = Contact.findOne({filePath:{$in:req.params.name}});
+  
+    res.set({'Content-Type': 'image/jpg'});
+    res.contentType('image/png');
+    res.contentType('image/jpg');
+    res.sendFile(__dirname+'/images/'+req.params.name);
+  
+  
+
+});
 
 module.exports = router;
